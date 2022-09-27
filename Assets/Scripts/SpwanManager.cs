1ea98935e1b182e30a4b4a583937bf8d60255172
S.A.V.I.O.U.R <S.A.V.I.O.U.R@SIMILLUSTRA.COM>
2022-09-27 14:01:47 +0100

    Unity Game Prototype -2

---
+++ Assets/Scripts/SpwanManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpwanManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", spawnInterval, startDelay);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetKeyDown(KeyCode.S))
+        // {
+        //     SpawnRandomAnimal();
+        // }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnpos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex], spawnpos,
+            animalPrefabs[animalIndex].transform.rotation);
+    }
+}
